0f08a1ae56cb908821bc8e4853b997c656967238
ddeerreekk <dyang1206@gmail.com>
2015-12-14 15:33:10 +0800

    updates

--- main.py
+++ main.py
@@ -8,26 +8,41 @@ import argparse
 import json
 import logging
 import dnslib
+import os
 from Crypto.PublicKey import RSA
 from hashlib import sha1
+
 
 MAX_SALT_BUFFER = 255
 certs = dict()
 recentsalt = []
+str=os.urandom(16)
 DEFAULT_REMOTE_HOST = "0.0.0.0"
 DEFAULT_REMOTE_PORT = 8000
 
-def decode(data):
-    msg = ''
-    tipo = (ord(data[2]) >> 3) & 15  
-    if tipo == 0:                   
-      ini = 12
-      lon = ord(data[ini])
-      while lon != 0:
-        msg += data[ini + 1:ini + lon + 1] + '.'
-        ini += lon + 1
-        lon = ord(data[ini])
-    return msg
+class certloader:
+
+    def __init__(self, cert_data):
+        self.cert_data = cert_data
+
+    # TODO: need to support more formats
+    # Return RSA key files
+    def importKey(self):
+        try:
+            return RSA.importKey(self.cert_data)
+        except Exception as err:
+            print ("Fatal error while loading certificate.")
+            print (err)
+            quit()
+
+    def getSHA1(self):
+        try:
+            return sha1(self.cert_data.encode("UTF-8")).hexdigest()
+        except Exception as err:
+            print ("Cannot get SHA1 of the certificate.")
+            print (err)
+            quit()
+
 
 def decrypt_udp_msg(msg1, msg2, msg3, msg4, msg5):
         """Return (main_pw, client_sha1, number).
@@ -44,10 +59,8 @@ def decrypt_udp_msg(msg1, msg2, msg3, msg4, msg5):
         """
         global recentsalt, certs, MAX_SALT_BUFFER
         assert len(msg1) == 46
-
         if msg5 in recentsalt:
             return (None, None, None, None, None)
-
         number_hex, port_hex, client_sha1 = msg1[:2], msg1[2:6], msg1[6:46]
         remote_ip = str(ipaddress.ip_address(int(msg4)))
         h = hashlib.sha256()
@@ -64,16 +77,39 @@ def decrypt_udp_msg(msg1, msg2, msg3, msg4, msg5):
                 number,
                 remote_port,
                 remote_ip]
+        logging.info("Data decrypted, processing")
         return returnvalue
         
 def process_msg(*msg):
-    print(msg)
-    #TODO: Assume the udptransmit server can load its own private and public key. Adapt the code in v0.1.1 to send a packet based on credentials of this server instead of the client. 
-    pass
+    main_pw, client_sha1, number, tcp_port, remote_ip=msg[0],msg[1],msg[2],msg[3],msg[4]
+    salt = os.urandom(16)
+    main_pw_hex="%X" % main_pw
+    required_hex = "%X" % min((number), 255)
+    sign_hex = '%X' % localpri.sign(salt, None)[0]
+    remote_ip_hex= "%X" % remote_ip
+    remote_port_hex = '%X' % tcp_port
+    if len(required_hex) == 1:
+        required_hex = '0' + required_hex
+    if len(sign_hex) == 510:
+        sign_hex = '0' + sign_hex
+    remote_port_hex = '0' * (4 - len(remote_port_hex)) + remote_port_hex
+    return  salt + \
+            bytes(main_pw_hex,"UTF-8") + \
+            bytes(client_sha1,"UTF-8") + \
+            bytes(required_hex, "UTF-8") + \
+            bytes(remote_ip_hex,"UTF-8") + \
+            bytes(remote_port_hex, "UTF-8") + \
+            bytes(localpub_sha1, "UTF-8") + \
+            bytes(sign_hex, "UTF-8") + \
+            remotecert.encrypt(str, None)[0]
+
+    #TODO: Assume the udptransmit server can load its own private and public key. Adapt the code in v0.1.1 to send a packet based on credentials of this server instead of the client.
+    # pass
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--config', dest="config", default='config.json')
+    parser.add_argument("-v", dest="v", action="store_true", help="show detailed logs")
     args = parser.parse_args()
 
     try:
@@ -85,6 +121,41 @@ if __name__ == "__main__":
         quit()
 
 
+    try:
+        remotecert_data = open(data["remote_cert"], "r").read()
+        remotecert = certloader(remotecert_data).importKey()
+    except KeyError as e:
+        logging.error(e.tostring() + "is not found in the config file. Quitting.")
+        quit()
+    except Exception as err:
+        print ("Fatal error while loading remote host certificate.")
+        print (err)
+        quit()
+
+    try:
+        localpri_data = open(data["local_cert"], "r").read()
+        localpri = certloader(localpri_data).importKey()
+        localpri_sha1 = certloader(localpri_data).getSHA1()
+        if not localpri.has_private():
+            print("Fatal error, no private key included in local certificate.")
+    except KeyError as e:
+        logging.error(e.tostring() + "is not found in the config file. Quitting.")
+        quit()
+    except Exception as err:
+        print ("Fatal error while loading local certificate.")
+        print (err)
+        quit()
+
+    try:
+        localpub_data = open(data["local_cert_pub"], "r").read()
+        localpub_sha1 = certloader(localpub_data).getSHA1()
+    except KeyError as e:
+        logging.error(e.tostring() + "is not found in the config file. Quitting.")
+        quit()
+    except Exception as err:
+        print ("Fatal error while calculating SHA1 digest.")
+        print (err)
+        quit()
     try:
         for client in data["clients"]:
             with open(client[0], "r") as f:
@@ -104,10 +175,19 @@ if __name__ == "__main__":
         quit()
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.bind(('', 53))
+    if args.v:
+        logging.basicConfig(level=logging.INFO)
     while 1:
         msg, addr = s.recvfrom(2048)
-        req = dnslib.DNSRecord.parse(msg)
+        if msg:
+            logging.info("Received request from"+addr)
+        try:
+            req = dnslib.DNSRecord.parse(msg)
+        except Exception as err:
+            logging.info("Corrupted request")
         reqdomain = str(req.q.qname)
         query_data = reqdomain.split('.')
-        s.sendto(process_msg(decrypt_udp_msg(query_data[0], query_data[1], query_data[2], query_data[3], query_data[4]), addr))
+        decrypted_msg=decrypt_udp_msg(query_data[0], query_data[1], query_data[2], query_data[3], query_data[4])
+        processed_msg=process_msg(decrypted_msg)
+        s.sendto(processed_msg, addr)
         # TODO: use logging to show logs about success and failures
